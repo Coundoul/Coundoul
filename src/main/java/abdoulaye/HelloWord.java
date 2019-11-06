3398c39ab03d9233002c32ca2d6ff743aa77fc90
Coundoul <coundoulabdoulaye471@gmail.com>
2019-11-06 15:37:26 +0000

    first commit

---
+++ src/main/java/abdoulaye/HelloWord.java
@@ -0,0 +1,19 @@
+package abdoulaye;
+
+public class HelloWord {
+	
+	private String message;
+	
+	public String getMessage() {
+		return message;
+	}
+	
+
+	public void test() {
+		System.out.println("Hello Word");
+	}
+	
+	public int sum(int a, int b) {
+		return a+b;
+	}
+}
